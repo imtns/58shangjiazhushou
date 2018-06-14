4597601fdde59631c96e55ae30f3a041986efe33
yaogang <yaogang@58ganji.com>
2018-06-14 18:49:01 +0800

    order manage

---
+++ dist/pages/OrderSuccess.js
@@ -0,0 +1,39 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+var _wepy = require('./../npm/wepy/lib/wepy.js');
+
+var _wepy2 = _interopRequireDefault(_wepy);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var OrderSuccess = function (_wepy$page) {
+    _inherits(OrderSuccess, _wepy$page);
+
+    function OrderSuccess() {
+        _classCallCheck(this, OrderSuccess);
+
+        return _possibleConstructorReturn(this, (OrderSuccess.__proto__ || Object.getPrototypeOf(OrderSuccess)).apply(this, arguments));
+    }
+
+    _createClass(OrderSuccess, [{
+        key: 'onLoad',
+        value: function onLoad() {}
+    }]);
+
+    return OrderSuccess;
+}(_wepy2.default.page);
+
+exports.default = OrderSuccess;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIk9yZGVyU3VjY2Vzcy5qcyJdLCJuYW1lcyI6WyJPcmRlclN1Y2Nlc3MiLCJ3ZXB5IiwicGFnZSJdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7QUFDQTs7Ozs7Ozs7Ozs7O0lBRXFCQSxZOzs7Ozs7Ozs7OztpQ0FDUCxDQUVUOzs7O0VBSHFDQyxlQUFLQyxJOztrQkFBMUJGLFkiLCJmaWxlIjoiT3JkZXJTdWNjZXNzLmpzIiwic291cmNlc0NvbnRlbnQiOlsiXHJcbmltcG9ydCB3ZXB5IGZyb20gJ3dlcHknO1xyXG5cclxuZXhwb3J0IGRlZmF1bHQgY2xhc3MgT3JkZXJTdWNjZXNzIGV4dGVuZHMgd2VweS5wYWdlIHtcclxuICAgIG9uTG9hZCAoKSB7XHJcblxyXG4gICAgfVxyXG59XHJcbiJdfQ==
