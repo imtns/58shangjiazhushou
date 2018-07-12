7b1088d40192de231cf207a0e03bcbe989c55869
zhouwunan <zhouwunan@58ganji.com>
2018-07-12 16:29:36 +0800

    店铺装修-优惠券选择，优惠券组件化

--- src/mixins/coupon.js
+++ src/mixins/coupon.js
@@ -1,4 +1,6 @@
 import wepy from 'wepy';
+import { LOAD_COUPON_LIST } from '../utils/url';
+import { get } from '../utils/ajax';
 
 export default class CouponMixin extends wepy.mixin {
     formatTime(v, type = 'YYYY-DD-MM') {
@@ -16,6 +18,50 @@ export default class CouponMixin extends wepy.mixin {
 
         if (type === 'YYYY-DD-MM') {
             result = time;
+        }
+
+        return result;
+    }
+
+    async getCoupons({ mpId, pageNum, pageSize }) {
+        let result = [];
+
+        let { data: coupons } = await get(LOAD_COUPON_LIST, {
+            pageNum,
+            pageSize,
+            mpId,
+        });
+
+        if (coupons && coupons.length) {
+            coupons = coupons.map(item => {
+                const coupon = Object.assign({}, item);
+                let { couponCondition } = coupon;
+                const { validStarttime, validEndtime, validType } = coupon;
+
+                if (validEndtime && (new Date(validEndtime)).getTime() - Date.now() < 0) {
+                    coupon.unable = true;
+                }
+
+                if (couponCondition) {
+                    try {
+                        couponCondition = JSON.parse(couponCondition);
+                        Object.assign(coupon, couponCondition);
+                    } catch (e) {
+                        console.log(e);
+                    }
+                }
+
+                if (validType === 1 && validStarttime && validEndtime) {
+                    Object.assign(coupon, {
+                        validStarttime: this.formatTime(validStarttime, 'YYYY.DD.MM'),
+                        validEndtime: this.formatTime(validEndtime, 'YYYY.DD.MM'),
+                    });
+                }
+
+                return coupon;
+            });
+
+            result = coupons;
         }
 
         return result;
