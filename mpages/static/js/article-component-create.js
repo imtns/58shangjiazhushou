c6c2d551ff3b162e4b2c436bc6b13d0e8835ea1d
heyanzhen@58ganji.com <heyanzhen>
2018-07-13 14:11:17 +0800

    文章模块，预约预览，收款记录

--- mpages/static/js/article-component-create.js
+++ mpages/static/js/article-component-create.js
@@ -49,18 +49,52 @@ var Page = {
                 });
             }
         });
+        // 上传头图
+        $('#item-file-btn').on('change', function() {
+            var self = this,
+            formData = new FormData();
+            formData.append('sources', $(self).get(0).files[0]);
+            $.ajax({
+                url: '/fileUpload',
+                type: 'POST',
+                data: formData,
+                contentType: false,
+                processData: false,
+                success: function(res) {
+                    var src= res.data.sources;
+                    if (src) {
+                        $('#cover').attr('src', "http://pic1.58cdn.com.cn" + src);
+                        $('.item-upload-img').removeClass('none');
+                        $('.item-file-div').addClass('none');
+                    } else {
+                        Page.pop('图片上传失败');
+                    }
+                    
+                }
+            });
+        });
+        // 重新上传
+        $('.exchange-img').on('click', function() {
+            $('.item-upload-img').attr('src', '').addClass('none');
+            $('.item-file-div').removeClass('none');
+        });
         $('.save-btn').on('click', function() {
             var group = Page.getKey('group'),
                 title = $('._title').val(),
                 intro = $('._content').find('p:first-child').text().substring(0, 20),
                 source = $('_source').val(),
                 author = $('._author').val(),
-                temp = $('._content' ).find();
-                content = $("._content").html();
+                temp = $('._content' ).find(),
+                content = $("._content").html(),
+                cover = $('#cover').attr('src') ? $('#cover').attr('src').split(".cn")[1].replace(/([/])\1+/g, "$1") : '';
             if (!title || title.length > 15) {
                 Page.pop("文章标题为空或多于15个字");
                 return;
             };
+            if (!cover) {
+                Page.pop("请上传文章头图");
+                return;
+            }
             if (temp.length == 1 && temp.html == '<br>') {
                 Page.pop("请编辑文章内容");
                 return;
--- src/utils/url.js
+++ src/utils/url.js
@@ -1,3 +1,4 @@
 export const LOAD_RESOURCE_URL = '/businessResource/getlist'; // 获取素材列表
 export const DELETE_RESOURCE = '/businessResource/delete/'; // 删除素材
 export const SAVE_RESOURCE_URL = '/businessResource/insert'; // 新增素材
+export const DELETE_ARTICLE_GROUP = '/businessArticle/delgroup/0'; // 删除文章分组
