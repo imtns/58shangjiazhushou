5966b4b4bdfdbbfded70153747e5dd52f6d52201
heyanzhen@58ganji.com <heyanzhen>
2018-07-26 09:42:58 +0800

    article bugs

--- mpages/static/js/add-version.js
+++ mpages/static/js/add-version.js
@@ -2,7 +2,7 @@ window.__staticConfig = {
     cssCdn: 'https://static.58.com',
     cssCfg:[{
         name:'css',
-        version: '20180720',
+        version: '20180725222',
         url:'/lbg/shangjiaxcxht/zhushou/css/article-component-create.css'
     }],
     jsCdn: 'https://static.58.com',
@@ -12,11 +12,11 @@ window.__staticConfig = {
         url:'/lbg/shangjiaxcxht/zhushou/js/zepto.min.js'
     },{
         name:'js',
-        version: '20180719',
+        version: '20180720',
         url:'/lbg/shangjiaxcxht/zhushou/js/editor.js'
     },{
         name:'js',
-        version: '20180724222',
+        version: '20180725111',
         url:'/lbg/shangjiaxcxht/zhushou/js/article-component-add.js'
     }]
 };
