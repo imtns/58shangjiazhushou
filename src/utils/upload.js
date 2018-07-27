18439fb5fb718d9744928ceeaaf734a45a28f0c5
huangxin11 <huangxin11@58ganji-corp.com>
2018-07-27 10:18:15 +0800

    视频页bug修复

--- src/utils/upload.js
+++ src/utils/upload.js
@@ -14,7 +14,6 @@ const uploadResource = async (path, type) => {
 };
 
 const uploadImages = async ({ count = 9, sourceType = ['album', 'camera'] } = {}) => {
-    console.log('--------------------------------!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
     const { tempFiles } = await wepy.chooseImage({
         count, sourceType,
     });
