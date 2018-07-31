bbb848d01da5f849caedf4e5663630d0bbe23665
heyanzhen@58ganji.com <heyanzhen>
2018-07-31 16:16:49 +0800

    bug fixed

--- mpages/static/js/add-version.js
+++ mpages/static/js/add-version.js
@@ -2,7 +2,7 @@ window.__staticConfig = {
     cssCdn: 'https://static.58.com',
     cssCfg:[{
         name:'css',
-        version: '20180725222',
+        version: '201807231',
         url:'/lbg/shangjiaxcxht/zhushou/css/article-component-create.css'
     }],
     jsCdn: 'https://static.58.com',
