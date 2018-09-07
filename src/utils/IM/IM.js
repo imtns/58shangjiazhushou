4212971f1b01e8fda9c02201dc5bd1deca13534e
heyanzhen@58ganji.com <heyanzhen>
2018-09-07 19:06:46 +0800

    chat

--- src/utils/IM/IM.js
+++ src/utils/IM/IM.js
@@ -1,22 +1,14 @@
 const webim = require('../../utils/IM/webim_wx.js');
 const { pubsub } = require('../../utils/IM/pubsub');
+import { globalData } from '../../utils/globalData';
 
 import { get } from '../../utils/ajax';
 import { CHAT_LOGIN } from '../../utils/url';
 
-export const IMlogin = (cb) => {
-    const { data } = get(CHAT_LOGIN, {
-        channel: 1, // channel 身份 1是名片 2是用户
-        userId: cardId, // userId 该身份对应的识别id，商家传cardId(名片Id)
-    });
-    console.log('*****', data);
-    cb && cb();
-}
-
 export const sdkLogin = (userInfo, listeners, options, callBack) => {
     webim.login(userInfo, listeners, options, (resp) => {
         //identifierNick为登录用户昵称(没有设置时，为帐号)，无登录态时为空
-        console.debug(identifierNick);
+        // console.debug(identifierNick);
         webim.Log.info('webim登录成功');
         callBack && callBack();
     }, (err) => {
@@ -44,3 +36,4 @@ export const onMsgNotify = (newMsgList) => {
         pubsub.publish(userId, obj[userId], +new Date());
     }
 }
+
