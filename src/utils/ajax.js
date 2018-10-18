e6f6349d73f975f870361f60e479765ba49b4912
yuanxiaodan <yuanxiaodan@58ganji.com>
2018-10-18 11:29:52 +0800

    test环境

--- src/utils/ajax.js
+++ src/utils/ajax.js
@@ -21,7 +21,7 @@ const http = (method, ...props) => new Promise((resolve, reject) => {
         delay = loadingControl.delay;
     }
     // test="test"字段是为切换测试和线上环境的，如果提交审核和发布，将test改为''，标识切换为线上环境
-    const sendData = Object.assign({}, data, { test: '' });
+    const sendData = Object.assign({}, data, { test: 'test' });
     // const sendData = Object.assign({}, data, { test: '' });
     // ppu加入header
     const ppu = wx.getStorageSync('ppu');
@@ -39,7 +39,7 @@ const http = (method, ...props) => new Promise((resolve, reject) => {
             PPU: ppu || 'wanghongyue',
             // 'YkuYdY8rk5As4T2QaJ7v': '45797966958100',
             reqfrom: 'biz_assistant',
-            'YkuYdY8rk5As4T2QaJ7v': 47350441209367,
+            // 'YkuYdY8rk5As4T2QaJ7v': 47350441209367,
         },
         success(response) {
             console.log('response', response);
