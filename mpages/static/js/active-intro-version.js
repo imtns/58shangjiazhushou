094a37e783e6afc3e328b9963543b2e95c1f1d7d
yuanxiaodan <yuanxiaodan@58ganji.com>
2018-10-19 16:48:09 +0800

    跳转客服消息公众号

--- mpages/static/js/active-intro-version.js
+++ mpages/static/js/active-intro-version.js
@@ -2,17 +2,17 @@ window.__staticConfig = {
     cssCdn: 'https://static.58.com',
     cssCfg:[{
         name:'css',
-        version: '20181015',
+        version: '20181019',
         url:'/lbg/shangjiaxcxht/zhushou/css/active-intro.css'
     }],
     jsCdn: 'https://static.58.com',
     jsCfg:[{
         name:'jq',
-        version: '20180717',
+        version: '20181019',
         url:'/lbg/shangjiaxcxht/zhushou/js/zepto.min.js'
     },{
         name:'js',
-        version: '20181015',
+        version: '20181019',
         url:'/lbg/shangjiaxcxht/zhushou/js/active-intro.js'
     }]
 };
