45005d3b24889a87c48f92065cc83b09a8ce1357
Jason <jason@JasondeMacBook-Pro.local>
2018-12-01 17:59:25 +0800

    bug fixed

--- mpages/static/js/goods-add-version.js
+++ mpages/static/js/goods-add-version.js
@@ -1,24 +1,24 @@
 /*eslint-disable */
 window.__staticConfig = {
-    cssCdn: 'https://c.58cdn.com.cn',
+    cssCdn: 'https://static.58.com',
     cssCfg: [{
         name: 'css',
-        version: '20181128',
+        version: '20181201',
         url: '/lbg/shangjiaxcxht/zhushou/css/goods-component-add.min.css',
     }],
     jsCdn: 'https://static.58.com',
     jsCfg: [{
         name: 'jq',
-        version: '20181128',
+        version: '20181201',
         url: '/lbg/shangjiaxcxht/zhushou/js/zepto.min.js',
     }, {
         name: 'js',
-        version: '20181128',
+        version: '20181201',
         url: '/lbg/shangjiaxcxht/zhushou/js/editor.js',
     },
     {
         name: 'js',
-        version: '20181128',
+        version: '20181201',
         url: '/lbg/shangjiaxcxht/zhushou/js/goods-component-add.js',
     }
 ],
